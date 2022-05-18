0559ec4a1acbb24e24d71c7c87cce5dc101dd64f
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-05-18 20:40:09 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,40 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistName: '',
+    buttonEnabled: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minChars = 2;
+    this.setState({
+      artistName: value,
+      buttonEnabled: value.length >= minChars,
+    });
+  }
+
   render() {
+    const { artistName, buttonEnabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search Page
+        <form>
+          <input
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            value={ artistName }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ !buttonEnabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
