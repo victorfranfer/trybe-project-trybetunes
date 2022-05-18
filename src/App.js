c8a8b4bcf1be2979f428ce71105ede61c77960b4
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-05-18 18:40:33 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <h1>TrybeTunes</h1>
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    userName: '',
+    buttonEnabled: false,
+    loading: false,
+    logged: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minChars = 3;
+    this.setState({
+      userName: value,
+      buttonEnabled: value.length >= minChars,
+    });
+  }
+
+  handleClick = async () => {
+    const { userName } = this.state;
+    this.setState(() => ({ loading: true }));
+    await createUser({ name: userName });
+    this.setState({ loading: false, logged: true });
+  }
+
   render() {
+    const { userName, buttonEnabled, loading, logged } = this.state;
+    if (logged) return <Redirect to="/search" />;
+    if (loading) return <Loading />;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="login-name-input">
+            Insira seu nome
+            <input
+              type="text"
+              name="login-name"
+              id="login-name-input"
+              data-testid="login-name-input"
+              value={ userName }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            id="login-submit-button"
+            data-testid="login-submit-button"
+            onClick={ this.handleClick }
+            disabled={ !buttonEnabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
